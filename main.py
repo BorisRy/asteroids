8b2aebfd65b623d0b4883cb51e0d56e7339db8f3
BorisRy <ryjkovboris@gmail.com>
2025-03-22 11:48:00 +0200

    added constants and player class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,15 +10,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         # check for user events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        # draw screen
+        # draw game(screen, player, etc)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        delta = clock.tick(60)
+        dt = delta / 1000
 
 if __name__ == "__main__":
     main()
