35bc32a3b9cddbcba4c39b34bc4a548462c59df4
BorisRy <ryjkovboris@gmail.com>
2025-03-22 12:26:37 +0200

    added asteroid field and end game on collision with asteroid

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
     
     while True:
         # check for user events
@@ -21,13 +26,26 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        # update game state
+        for element in updatable:
+            element.update(dt)
+            
+        # check if any asteroids collide with player
+        for asteroid in asteroids:
+            if asteroid.is_collision(player):
+                print("Game Over!")
+                return
+        
         # draw game(screen, player, etc)
         screen.fill("black")
-        player.draw(screen)
+        
+        for element in drawable:
+            element.draw(screen)
+            
         pygame.display.flip()
         
-        delta = clock.tick(60)
-        dt = delta / 1000
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
